98a4a29b0008856ce722c6f3d86a772baf557145
Unknown <jimena_cab@hotmail.com>
2018-04-10 10:15:54 +0200

    First version

---
+++ mqtt-server.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+""""
+Running HBMQTT as MQTT broker.
+
+Copyright (c) 2015-2018, Fabian Affolter <fabian@affolter-engineering.ch>
+Released under the MIT license. See LICENSE file for details.
+
+Source: https://github.com/beerfactory/hbmqtt/blob/develop/samples/broker_start.py
+"""
+import asyncio
+import logging
+
+from hbmqtt.broker import Broker
+
+logger = logging.getLogger(__name__)
+
+config = {
+    'listeners': {
+        'default': {
+            'type': 'tcp',
+            'bind': '79.153.157.196:1883',
+        },
+        'ws-mqtt': {
+            'bind': '79.153.157.196:9001',
+            'type': 'ws',
+            'max_connections': 10,
+        },
+    },
+}
+
+broker = Broker(config)
+
+
+@asyncio.coroutine
+def test_coro():
+    yield from broker.start()
+
+
+if __name__ == '__main__':
+    formatter = "[%(asctime)s] :: %(levelname)s :: %(name)s :: %(message)s"
+    logging.basicConfig(level=logging.INFO, format=formatter)
+    asyncio.get_event_loop().run_until_complete(test_coro())
+    asyncio.get_event_loop().run_forever()
